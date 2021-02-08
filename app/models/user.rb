849510145916b09e3dd19b705a46dd9f65fa2c7f
Samuel Pedro <redsamuel.cruz7@gmail.com>
2021-02-08 14:37:20 +0100

    Fix a syntax error

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :username, uniqueness: true, length: { minimum: 1 }
     validates :email, uniqueness: true
-    validates :password, length: { in 8..32 }
+    validates :password, length: { in: 8..32 }
   end
 end
