5eb7339b999bd18ef6325d92ae2e7cd0248eb12c
Samuel Pedro <redsamuel.cruz7@gmail.com>
2021-02-08 14:07:28 +0100

    Add user model

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  with_options presence: true do
+    validates :username, uniqueness: true, length: { minimum: 1 }
+    validates :email, uniqueness: true
+    validates :password, length: { in 8..32 }
+  end
+end
